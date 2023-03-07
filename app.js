93917cb02515f98aedc9aae4264a94d5ab82b771
aleksandar-code <83082486+aleksandar-code@users.noreply.github.com>
2023-03-07 20:44:55 +0100

    Can get winner

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const ticTacToe = () => {
   const Player = (id) => {
     const symbol = id === 0 ? "X" : "O";
-
     return { symbol, id };
   };
 
@@ -11,11 +10,18 @@ const ticTacToe = () => {
       ["", "", ""],
       ["", "", ""]
     ];
-
     const arrayOfCells = [];
-    const getCell = (cell) => {
-      arrayOfCells.push(cell);
-    };
+    const track = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [2, 4, 6],
+      [0, 4, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8]
+    ];
+
     const createBoard = () => {
       const gameBoard = doc.getElementById("game");
       board.forEach((row) => {
@@ -27,7 +33,7 @@ const ticTacToe = () => {
           boardCell.classList.add("board-cell");
           const cellContent = document.createElement("p");
           cellContent.textContent = col;
-          getCell(boardCell);
+          arrayOfCells.push(boardCell);
           boardRow.appendChild(boardCell);
           boardCell.appendChild(cellContent);
         });
@@ -36,9 +42,16 @@ const ticTacToe = () => {
 
     const playSymbolAt = (symbol, index) => {
       arrayOfCells[index].textContent = symbol;
+      track.forEach((array, idx1) => {
+        array.forEach((element, idx2) => {
+          if (element === index) {
+            track[idx1][idx2] = symbol;
+          }
+        });
+      });
     };
 
-    return { createBoard, arrayOfCells, playSymbolAt };
+    return { track, createBoard, arrayOfCells, playSymbolAt };
   })(document);
 
   const Game = () => {
@@ -46,16 +59,30 @@ const ticTacToe = () => {
     const players = [Player(0), Player(1)];
     let i = 0;
     let currentPlayer = players[i];
+
     const switchPlayer = () => {
       i = i === 0 ? 1 : 0;
       currentPlayer = players[i];
     };
 
     board.createBoard();
+
+    const checkWin = () => {
+      const sym = currentPlayer.symbol;
+
+      board.track.forEach((combo) => {
+        if (combo.toString() === [sym, sym, sym].toString()) {
+          console.log(currentPlayer, "wins");
+        }
+      });
+      console.log(board.track);
+    };
+
     const listenClick = () => {
       board.arrayOfCells.forEach((cell, index) => {
         cell.addEventListener("click", () => {
           board.playSymbolAt(currentPlayer.symbol, index);
+          checkWin();
           switchPlayer();
         });
       });
